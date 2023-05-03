1a54064a4240b4b5eb96202b2d17beb357f66dcc
rayhan kabir <rayhan.kabir29@gmail.com>
2023-05-03 12:35:30 +0600

    initial commit

---
+++ src/model/FullList.ts
@@ -0,0 +1,20 @@
+import ListItem  from "./ListItem";
+
+interface List {
+   list : ListItem[],
+   load(): void,
+   save():void,
+   clearList():void,
+   addItem(itemObj : ListItem): void,
+   removeIte(id:string): void,
+}
+
+export default class FullList implements List {
+    static instance : FullList = new FullList();
+    private constructor(private _list: ListItem[] = [] )
+    {}
+    get list(): ListItem[]{
+        return this._list;
+    }
+
+}
