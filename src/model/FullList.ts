688982dd7419b36f49af22fcbc228df653850203
rayhan kabir <rayhan.kabir29@gmail.com>
2023-05-03 14:54:14 +0600

    models

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -1,20 +1,48 @@
-import ListItem  from "./ListItem";
+import ListItem from "./ListItem";
 
 interface List {
-   list : ListItem[],
-   load(): void,
-   save():void,
-   clearList():void,
-   addItem(itemObj : ListItem): void,
-   removeIte(id:string): void,
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeIte(id: string): void;
 }
 
 export default class FullList implements List {
-    static instance : FullList = new FullList();
-    private constructor(private _list: ListItem[] = [] )
-    {}
-    get list(): ListItem[]{
-        return this._list;
-    }
-
+  static instance: FullList = new FullList();
+  private constructor(private _list: ListItem[] = []) {}
+  get list(): ListItem[] {
+    return this._list;
+  }
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myList");
+    if (typeof storedList !== "string") return;
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList);
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      )
+      FullList.instance.addItem(newListItem)
+      ;
+    });
+  }
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list));
+  }
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+  removeIte(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id);
+    this.save();
+  }
 }
